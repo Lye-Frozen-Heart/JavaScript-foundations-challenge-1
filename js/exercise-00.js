54e4b601cc28cb3e375d37882cb0e8ecce5192ec
LyesFrozenHeart <torresbravoalex@gmail.com>
2023-10-17 21:07:06 +0200

    Done the exercises waiting to merge

---
+++ js/exercise-00.js
@@ -0,0 +1,22 @@
+/* 
+   Create a constant variable of each of the following primitive types:
+   - String
+   - Number
+   - Boolean
+   - Null
+   - Undefined
+
+   Log out each variable to the terminal at the end.
+*/
+const stringVariable = "Hello";
+console.log({stringVariable});
+const numberVariable = 8;
+console.log({numberVariable});
+const booleanVariable = true;
+console.log({booleanVariable});
+const nullVariable = null;
+console.log({nullVariable});
+const undefinedVariable = undefined;
+console.log({undefinedVariable});
+console.log("Exc 00:---> finished");
+
---
+++ js/exercise-02.js
@@ -0,0 +1,18 @@
+/* 
+    1. Declare a variable using let with any name you like, but without a value.
+    2. On a separate line, assign the variable the String: "Moo!"
+    3. Log out this variable to the terminal using console.log
+    
+    Repeat the above steps but try using a const variable. Comment this new part of code and explain what is happening.
+*/
+let unassignedLet; 
+unassignedLet = "Moo!";
+console.log(unassignedLet);
+
+/*
+*const unassignedConstant; 
+*unassignedVariable = "Moo!";
+*console.log(unassignedVariable);
+*Constants must be initialized at first and also can't be changed/reassigned! 
+*/
+console.log("Exc 02:---> finished");
